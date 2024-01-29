0394d60e4de932c133705a1d69b7814229054103
whoami <137587937+jxrDEV@users.noreply.github.com>
2024-01-29 09:44:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = () => {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+  };
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '300 pages', 'not read yet');
+console.log(theHobbit.info());
+
+function addBookToLibrary() {
+  
+}
